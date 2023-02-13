f46d1ae240ad956ef381d5d7ba4ddc2f7d679fab
Brad Hanks <hanks.brad@gmail.com>
2023-02-13 13:45:04 -0700

    created listen socket

--- lib/protohacker/echo_server.ex
+++ lib/protohacker/echo_server.ex
@@ -10,7 +10,29 @@ defmodule Protohacker.EchoServer do
 
 @impl true
 def init(:no_state) do
-    Logger.info("Starting echo server.")
+    Logger.info("Starting echo server on port 5001.")
+
+    listen_options = [
+      mode: :binary,
+      active: false,
+      reuseaddr: true
+    ]
+
+    case :gen_tcp.listen(5001, listen_options) do
+      {:ok, listen_socket} ->
+        state = %__MODULE__{listen_socket: listen_socket}
+        {:ok, state}
+
+      {:error, reason} ->
+        {:stop, reason}
+    end
+
     {:ok, %__MODULE__{}}
+
   end
 end
+
+#For TCP -> listen socket: that's what you bind to a port on your machine
+# To accept a connection TCP libraries provide "accept"
+# When listen socket accepts a connection returns a peer socket.
+# Listen can accept many sockets, peer sockets are always one-to-one
